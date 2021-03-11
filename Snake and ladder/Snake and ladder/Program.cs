0c3afa5c75ae2b9a8fe593813c6c2c439e9b5352
keerthana31999 <keerthanacherry123@gmail.com>
2021-03-11 11:51:28 +0530

    Added::enter the player name

--- Snake and ladder/Snake and ladder/Program.cs
+++ Snake and ladder/Snake and ladder/Program.cs
@@ -4,15 +4,20 @@ namespace Snake_and_ladder
 {
     class Program
     {
-
         public const int START_POINT = 0;
         public const int END_POINT = 100;
         static void Main(string[] args)
         {
-            Console.WriteLine(" Welcome To Snake And Lader Problem and Enter player name");
+            Console.WriteLine(" Welcome To Snake And Lader Problem Enter player name");
             string player1 = Console.ReadLine();
-            int playerPosition = START_POINT;
-            Console.WriteLine("Current Position : " + playerPosition);
+            int diceRoll = DiceRoll();
+            Console.WriteLine("Dice Roll : " + diceRoll);
+        }
+        static int DiceRoll()
+        {
+            Random random = new Random();
+            int diceNum = random.Next(1, 7);
+            return diceNum;
         }
     }
 }
