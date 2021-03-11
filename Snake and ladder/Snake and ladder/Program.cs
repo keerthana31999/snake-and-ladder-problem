d71613b372509fbe2c2935cf3878ab756e0e19f5
keerthana31999 <keerthanacherry123@gmail.com>
2021-03-11 11:43:52 +0530

    Added::welcome to snake and ladder problem

---
+++ Snake and ladder/Snake and ladder/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Snake_and_ladder
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("welcome to snake and ladder problem");
+        }
+    }
+}
